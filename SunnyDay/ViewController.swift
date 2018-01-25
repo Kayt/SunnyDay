9ab5895659b0191d048fcc0b1c4b1d6034682cc8
Achim Munene <achim_munene@hotmail.com>
2018-01-25 13:43:35 +0200

    Initial Commit

---
+++ SunnyDay/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  SunnyDay
+//
+//  Created by Achim Munene on 25/1/2018.
+//  Copyright © 2018 Achim Munene. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
